6c14cca0dd78b73988d9fec39adbcdd88340a710
Vitor Ferreira da Cunha <172675791+Vitorferreiracunha@users.noreply.github.com>
2024-06-13 20:31:28 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Html-Css
+ Estudos em HTML5 e CSS3
---
+++ ex001a/script.js
@@ -0,0 +1,18 @@
+const tarefa = document.querySelector("#tarefa");
+const btn = document.querySelector("#btn");
+const lista = document.querySelector("#lista");
+
+btn.addEventListener("click", function(){
+    if(tarefa.value == ""){
+        alert('Digite uma tarefa válida!!')
+    } else {
+        lista.innerHTML += `<li>
+                                <i class="fas fa-check-circle check"></i>
+                                <span>${ tarefa.value }</span>
+                                <i class="fa-solid fa-trash-can close"></i>
+                            </li>`
+    }
+    tarefa.value = "";
+
+    
+})
